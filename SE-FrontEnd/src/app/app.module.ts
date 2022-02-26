2abaad95a07f509585f87744bd6e0317f32b918d
alexmanda2000 <alexmanda2000@yahoo.com>
2022-02-26 12:20:15 +0200

    Added angular material and homepage with router links.

--- SE-FrontEnd/src/app/app.module.ts
+++ SE-FrontEnd/src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes,RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+import { OperationsComponent } from './operations/operations.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+
+const appRoutes: Routes=[
+  {path:'home', component: HomeComponent},
+  {path: ''   , component: HomeComponent},
+  {path:'configuration', component: ConfigurationComponent},
+  {path:'operations', component: OperationsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    OperationsComponent,
+    ConfigurationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
